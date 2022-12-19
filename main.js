234652d34a7a77f9fe605eaf6aeaf55e1fbf4692
cengo12 <57234794+cengo12@users.noreply.github.com>
2022-12-19 21:29:06 +0300

    Add playGame()

--- main.js
+++ main.js
@@ -1,37 +1,49 @@
-const options = ["Rock", "Paper", "Scissors"];
-
-function getComputerChoice(list) {
-    return options[Math.floor(Math.random()*list.length)];
+function getComputerChoice() {
+    const options = ["Rock", "Paper", "Scissors"];
+    return options[Math.floor(Math.random()*options.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
     // your code here!
     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()){
-        return "It's a draw!"
+        return 1
     }
     else if(playerSelection.toLowerCase()=="rock" && computerSelection.toLowerCase()== "paper"){
-        return "Computer wins!"
+        return 0
     }
     else if(playerSelection.toLowerCase()=="paper" && computerSelection.toLowerCase()== "rock"){
-        return "Player wins!"
+        return 1
     }
     else if(playerSelection.toLowerCase()=="paper" && computerSelection.toLowerCase()== "scissors"){
-        return "Computer wins!"
+        return 0
     }
     else if(playerSelection.toLowerCase()=="rock" && computerSelection.toLowerCase()== "scissors"){
-        return "Player wins!"
+        return 1
     }
     else if(playerSelection.toLowerCase()=="scissors" && computerSelection.toLowerCase()== "paper"){
-        return "Player wins!"
+        return 1
     }
     else if(playerSelection.toLowerCase()=="scissors" && computerSelection.toLowerCase()== "rock"){
-        return "Computer wins!"
+        return 0
     }
     else{
         return "Wrong input"
     }
-  }
+}
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice(options);
-console.log(playRound(playerSelection, computerSelection));
+function playGame(){
+    let human_score = 0
+    for (let i = 0; i < 5; i++) {
+        let user_input = prompt("Make choice");
+        let round = playRound(user_input,getComputerChoice())
+        console.log(round)
+        if (round == 1){
+            ++human_score
+        };
+     }
+
+    let computer_score = 5-human_score
+    console.log(human_score,computer_score)
+}
+
+playGame()
